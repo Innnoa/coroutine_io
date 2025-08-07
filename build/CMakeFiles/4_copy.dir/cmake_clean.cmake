d6ad93e4a5a835c414e087576b30880938850ae6
innno <banxiaqaq@gmail.com>
2025-08-07 13:49:03 +0800

    1

---
+++ build/CMakeFiles/4_copy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "4_copy"
+  "4_copy.pdb"
+  "CMakeFiles/4_copy.dir/main.cpp.o"
+  "CMakeFiles/4_copy.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/4_copy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
